cab0a07dbc56258481f864926a28e38e0c18a19a
SovanChhaya <chhormsovanchhaya@gmail.com>
2018-04-30 21:42:24 +0700

    lab13 problem1

--- src/main/java/handong/edu/csee/java/Lab13/Prob01/SchoolFriend.java
+++ src/main/java/handong/edu/csee/java/Lab13/Prob01/SchoolFriend.java
@@ -2,7 +2,7 @@ package handong.edu.csee.java.Lab13.Prob01;//package name of Lab13 problem 1
 
 public class SchoolFriend extends Friend  {// SchoolFriend is the subClass of Friend class(Super class)
 
-	public void schhoolFriend()// method as void no return value just print out
+	public void schoolFriend()// method as void no return value just print out
 	{
 		System.out.println("School Friend!");// print out School Friend!
 	}
