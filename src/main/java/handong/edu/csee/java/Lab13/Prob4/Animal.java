a8b492f66ef52984adb4da99021c841a96e788e1
SovanChhaya <chhormsovanchhaya@gmail.com>
2018-05-01 16:24:19 +0700

    Lab03 problem04

--- src/main/java/handong/edu/csee/java/Lab13/Prob4/Animal.java
+++ src/main/java/handong/edu/csee/java/Lab13/Prob4/Animal.java
@@ -1,12 +1,12 @@
 package handong.edu.csee.java.Lab13.Prob4;// package name of problem 4 
 
 public class Animal {// class for input the name of animals as Supper Class
-	
+
 	private String nameOfAnimal;// instance variable as string
-	
+
 	public void setName(String name)// method for input name of animal
 	{
-	nameOfAnimal = name;//local variable
+		nameOfAnimal = name;//local variable
 	}
 	public void getName()// method for get a name from input
 	{
--- src/main/java/handong/edu/csee/java/Lab13/Prob4/Cat.java
+++ src/main/java/handong/edu/csee/java/Lab13/Prob4/Cat.java
@@ -2,10 +2,10 @@ package handong.edu.csee.java.Lab13.Prob4; // package name of problem 4
 
 // sub Class of Animal class
 public class Cat extends Animal implements Pet{// interface implements 
-	
+
 	public String food()// method as string to store the food of Cat
 	{
-	return "(Cat!)" +"Fish";// give the food name fish to cat when user input name of cat
+		return "(Cat!)" +"Fish";// give the food name fish to cat when user input name of cat
 	}
 
 
