a4c4acc037e86df6975eee4c9ee32491665a8e76
SovanChhaya <chhormsovanchhaya@gmail.com>
2018-05-01 16:56:36 +0700

    lab13 problem05

--- src/main/java/handong/edu/csee/java/Lab13/Prob5/MainProb5.java
+++ src/main/java/handong/edu/csee/java/Lab13/Prob5/MainProb5.java
@@ -1,13 +1,13 @@
 package handong.edu.csee.java.Lab13.Prob5; // package name for Lab13 problem 5
 import java.util.Scanner; // import scanner package for key board input
 
-public class MainProb5 {
+public class MainProb5 {// main class top print out the result
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int n1, n2 = 0;// local variable
-		Scanner keyboard = new Scanner(System.in);
-		
+		Scanner keyboard = new Scanner(System.in);// scan keyboard when input something
+
 		System.out.print("Enter row size(maximum 5): ");// print out Enter row size(maximum 5):
 		n1 = keyboard.nextInt();// input number of row
 		System.out.print("Enter column size(maximum 5): ");//print out Enter column size(maximum 5):
@@ -19,30 +19,32 @@ public class MainProb5 {
 		n2 = keyboard.nextInt();//input the size of column
 		int[][] m2 = new int[n1][n2];// instantiate
 		System.out.print("Enter List1: ");//print out Enter List1
-		for (int i = 0; i < m1.length; i++)
-			for (int j = 0; j < m1[i].length; j++)
+
+		for (int i = 0; i < m1.length; i++)// for  i smaller than number of length then i increase
+			for (int j = 0; j < m1[i].length; j++)// for  j smaller than array of length then j increase
 				m1[i][j] = keyboard.nextInt();// input the list of number
 
 		System.out.print("Enter list2: ");// print out Enter list2
-		for (int i = 0; i < m2.length; i++){
-			for (int j = 0; j < m2[i].length; j++)
+		for (int i = 0; i < m2.length; i++){// for i smaller than second number of length then i increase 
+			for (int j = 0; j < m2[i].length; j++)// for j smaller than number of array two then j increase
 				m2[i][j] = keyboard.nextInt();// input the list of number
 		}
-		for (int i = 0; i < m1.length; i++){
-			for (int j = 0; j < m1[i].length; j++)
-				System.out.print(m1[i][j] + " ");
+		for (int i = 0; i < m1.length; i++){// for i =0 and smaller than length then i increase
+			for (int j = 0; j < m1[i].length; j++)// for j = 0 and j smaller than length of array then j increase 
+				System.out.print(m1[i][j] + " ");// print out the result 
 			System.out.println();// print out the result 
 		}
 		System.out.println(); // print out the result
-		for (int i = 0; i < m2.length; i++){
-			for (int j = 0; j < m2[i].length; j++)
-				System.out.print(m2[i][j] + " ");
-			System.out.println();
+		for (int i = 0; i < m2.length; i++){// for i = 0 and i smaller than length of m2 i increase 
+			for (int j = 0; j < m2[i].length; j++)// for j = 0 and j smaller than length of array m2 then j increase 
+
+				System.out.print(m2[i][j] + " ");// show the list 
+			System.out.println();// print line 
 		}
 		System.out.println();// print out the result
 
 
-		if (equals(m1, m2)) 
+		if (equals(m1, m2)) // check the value of m1 and m2 
 			System.out.println("The two arrays are approximately identical.");// print out The two arrays are approximately identical.
 		else 
 			System.out.println("The two arrays are not identical"); // print out The two arrays are not identical
@@ -56,5 +58,7 @@ public class MainProb5 {
 		// TODO Auto-generated method stub
 		return false;
 	}
+
+
 
 }
--- src/main/java/handong/edu/csee/java/Lab13/Prob5/Prob5.java
+++ src/main/java/handong/edu/csee/java/Lab13/Prob5/Prob5.java
@@ -1,22 +1,24 @@
 package handong.edu.csee.java.Lab13.Prob5;// package name for Lab13 problem 5
 
 public class Prob5 {//class for Problem5 
+
 	public static boolean equals(int[][] m1, int[][] m2) {// method that store array two dementional
+
 		int count = 0;// local variable
 		if (m1.length != m2.length) // if the number of row is different, return false
 			return false;
-		for (int i = 0; i < m1.length; i++) // for 
+		for (int i = 0; i < m1.length; i++) // for i = 0 and i smaller then length of m1 then i increase 
 		{
-			for (int j = 0; j < m1[i].length; j++)
+			for (int j = 0; j < m1[i].length; j++)// for j = 0 and j smaller than length of array m1 then j increase 
 			{
-				if (m1[i][j] != m2[i][j])
+				if (m1[i][j] != m2[i][j])// if array of m1 not equal to array of m2 
 					count++;
 			}
 		}
-		if(count <= 3)
-			return true;
+		if(count <= 3)//if count smaller or equal to 3 
+			return true;// return true 
 		else
-			return false;
+			return false;// alse return fale 
 	}
 
 
